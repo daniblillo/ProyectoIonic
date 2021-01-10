e7c1779442861a9c81139e68de844115282e74b2
daniblillo <daniel.10.betico@gmail.com>
2021-01-10 19:22:25 +0100

    Act

--- myApp/src/models/peliculas.interface.ts
+++ myApp/src/models/peliculas.interface.ts
@@ -1,8 +1,10 @@
 export interface PeliculasInterfaz {
     id?: string;
-    titulo?: string;
-    director?: string;
-    sinopsis?: string;
-    clasificacion?: string;
+    Titulo?: string;
+    Genero?: string;
+    Duracion?: string;
+    Director?: string;
+    Descripcion?: string;
+    Calificacion?: string;
     img: string;
 }
